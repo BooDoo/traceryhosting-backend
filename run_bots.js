6119c6935a2e07e6f7e77e3e2d87aa7aa126f0e6
Joel McCoy <2465283+BooDoo@users.noreply.github.com>
2018-09-02 18:55:09 -0400

    use tab indents consistently

--- run_bots.js
+++ run_bots.js
@@ -29,8 +29,8 @@ const request = require('request');
 _.mixin({
 	guid : function(){
 	  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-	    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-	    return v.toString(16);
+		var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+		return v.toString(16);
 	  });
 	}
 });
@@ -206,7 +206,7 @@ var matchBrackets = function(text) {
   
   // simple utility function
   function reverseString(s) {
-    return s.split('').reverse().join('');
+	return s.split('').reverse().join('');
   }
 
   // this is an inverstion of the natural order for this RegEx:
@@ -215,10 +215,10 @@ var matchBrackets = function(text) {
   text = reverseString(text);
   var matches = text.match(bracketsRe);
   if(matches === null) {
-    return null;
+	return null;
   }
   else {
-    return matches.map(reverseString).reverse().map(prepareTag);
+	return matches.map(reverseString).reverse().map(prepareTag);
   }
 }
 
@@ -228,7 +228,7 @@ function removeBrackets (text) {
   
   // simple utility function
   var reverseString = function(s) {
-    return s.split('').reverse().join('');
+	return s.split('').reverse().join('');
   }
 
   // this is an inverstion of the natural order for this RegEx:
